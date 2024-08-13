3b3aa81b875536805c9042043e4951559a7eb8db
Alexander Petree <apetree1001@email.phoenix.edu>
2024-08-13 03:06:23 -0700

    Update MyCollectible.sol

--- contracts/MyCollectible.sol
+++ contracts/MyCollectible.sol
@@ -1,22 +1,9 @@
-pragma solidity ^0.5.11;
-
-import "./ERC1155Tradable.sol";
-
-/**
- * @title MyCollectible
- * MyCollectible - a contract for my semi-fungible tokens.
- */
-contract MyCollectible is ERC1155Tradable {
-  constructor(address _proxyRegistryAddress)
-  ERC1155Tradable(
-    "MyCollectible",
-    "MCB",
-    _proxyRegistryAddress
-  ) public {
-    _setBaseMetadataURI("https://creatures-api.opensea.io/api/creature/");
-  }
-
-  function contractURI() public view returns (string memory) {
-    return "https://creatures-api.opensea.io/contract/opensea-erc1155";
-  }
+pragma solidity ^0.5.11; import "./ERC1155Tradable.sol"; /***@title 
+MyCollectible*MyCollectible - a contract for 
+my semi-fungible tokens.*/contract MyCollectible is 
+ERC1155Tradable {constructor(address_proxyRegistryAddress)ERC1155
+Tradable("MyCollectible","MCB",_proxy
+RegistryAddress)public{_setBaseMetadataURI("https://creatures-api.opensea.io/api/creature/");} 
+function contractURI()
+public view returns(string memory){return "https://creatures-api.opensea.io/contract/opensea-erc1155";}
 }
