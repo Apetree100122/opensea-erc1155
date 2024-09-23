3c4131e94c28d88c1c276c647f333a19f61b2c24
Alexander Petree <apetree1001@email.phoenix.edu>
2024-09-23 03:27:48 -0700

    Update flatten.sh

--- flatten.sh
+++ flatten.sh
@@ -1,6 +1,11 @@
-./node_modules/.bin/truffle-flattener contracts/MyCollectible.sol
-> flattened/MyCollectible.sol./node_modules/.bin/truffle-flattener contracts/MyFactory.sol >
-flattened/MyFactory.sol./node_modules/.bin/truffle-flattener
-contracts/MyLootBox.sol > flattened
-
-[ >/MyLootBox.sol ]
+./node_modules
+   /.bin/truffle-flattener 
+       contracts/MyCollectible.sol> flattened/MyCollectible.sol.
+  /node_modules/.bin/truffle-flattener
+      contracts/MyFactory.sol >
+flattened/MyFactory.sol.
+    /node_modules
+    /.bin/truffle-flattener 
+    contracts/MyLootBox.sol >flattened [ >
+    /MyLootBox.sol 
+    ]
